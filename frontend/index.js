eacfb7f3e7aaf32501498777447da672c87eb720
Chacha <chachazw@gmail.com>
2022-07-01 23:22:02 +0800

    Update index.js

--- frontend/index.js
+++ frontend/index.js
@@ -1,14 +1,20 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-import { initContract } from './assets/js/near/utils'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
 
-const container = document.querySelector('#root')
-const root = createRoot(container) // createRoot(container!) if you use TypeScript
+import App from "./App";
+import { initContract } from "./assets/js/near/utils";
+
+const container = document.querySelector("#root");
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
 window.nearInitPromise = initContract()
   .then(() => {
-    <App />
-    root.render(<App tab="home" />)
+    <App />;
+    root.render(
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
+    );
   })
-  .catch(console.error)
+  .catch(console.error);
